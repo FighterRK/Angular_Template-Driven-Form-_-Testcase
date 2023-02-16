679f84b2c53296e8d208cf3dc92b54908e104729
Rahul <rahulksuresh370@gmail.com>
2023-02-16 16:39:47 +0530

    angular testcase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-driven-form-app';
+
+  onSubmit(data : NgForm){
+    console.log(data.value);
+    
+  }
+  onClick(datas: NgForm){
+   alert(JSON.stringify(datas.value,null,6))
+    
+
+  }
 }
